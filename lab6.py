87518f1125f3811daadadc5d07fd4894b6d98dc5
Kishor Maski <103872259+kishormaskikm@users.noreply.github.com>
2022-05-23 16:29:42 +0530

    Add files via upload

---
+++ lab6.py
@@ -0,0 +1,81 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self):
+
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+
+    def DFSUtil(self, v, visited):
+
+        visited.add(v)
+        print(v, end=' ')
+
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited)
+
+
+    def DFS(self, v):
+        visited = set()
+        self.DFSUtil(v, visited)
+
+# Driver code
+# Create a graph given # in the above diagram
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print("Following is DFS from (starting from vertex 2)")
+g.DFS(2)
+
+
+# Python3 Program to print BFS traversal # from a given source vertex. BFS(int s)
+# traverses vertices reachable from s.
+from collections import defaultdict
+# This class represents a directed graph # using adjacency list representation
+class Graph:
+
+    def init(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self,u,v):
+        self.graph[u].append(v)
+
+# Function to print a BFS of graph
+    def BFS(self, s):
+        visited = [False] * (max(self.graph) + 1)
+        queue = []
+        queue.append(s)
+        visited[s] = True
+        while queue:
+            s = queue.pop(0)
+            print(s, end=" ")
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+# Driver code
+
+
+# Create a graph given in # the above diagram
+
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print("Following is Breadth First Traversal" " (starting from vertex 2)")
+g.BFS(2)
